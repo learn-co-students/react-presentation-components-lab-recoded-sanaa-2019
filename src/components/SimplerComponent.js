503d9edd07989ac2366daddeb384f83ba21d018b
EmanAlzariey <eman_alzariey@yahoo.com>
2019-11-30 09:37:07 -0800

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,6 +1,11 @@
 // Code SimplerComponent Here
 import React from "react";
-const SimplerComponent = ({ handleClick }) => (
-  <div onClick={handleClick}>I am just happy</div>
-);
+const SimplerComponent = ({ handleClick }) => {
+
+  return( <div onClick={handleClick}>I am just happy</div>)
+ // handleClick is changed at index.js => it is passed as a callback prop
+}
+
 export default SimplerComponent;
+
+
