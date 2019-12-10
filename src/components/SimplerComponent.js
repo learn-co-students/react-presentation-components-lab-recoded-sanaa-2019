7a6fbe3658e0ab4311fa591b960de7f0d6dd28ba
HanaaEskander <55395559+HanaaEskander@users.noreply.github.com>
2019-12-10 09:27:45 +0300

    Update SimplerComponent.js

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,21 @@
 // Code SimplerComponent Here
+import React, { Component } from "react";
+const SimplerComponent = ({ handleClick }) => {
+    return (
+      <div onClick={handleClick}>
+      I am just happy
+      </div>
+    );
+}
+export default SimplerComponent;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SimpleComponent from './components/SimpleComponent';
+import SimplerComponent from './components/SimplerComponent';
+ReactDOM.render(
+  <div>
+    <SimpleComponent />
+    <SimplerComponent handleClick={()=>alert('hhhhhhhhh')} />
+  </div>,
+  document.getElementById('root')
+);
